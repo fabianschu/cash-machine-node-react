a6ad012c9dce790fe8351e01387380616af49dea
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-10-13 23:37:28 +0200

    adds country select

--- client/src/components/InputField.js
+++ client/src/components/InputField.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { useField } from "formik";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const InputField = (props) => {
   const { name } = props.field;
   const [field, meta, helpers] = useField(name);
   const { setValue } = helpers;
   const { value } = field;
+  const classes = useStyles();
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   const getLabel = () => {
     if (name === "firm") return "Firma";
@@ -25,9 +46,34 @@ const InputField = (props) => {
     if (name === "taxId") return "Steuernummer";
   };
 
+  const getType = () => {
+    if (name === "password") return "password";
+    if (name === "hourlyRate") return "number";
+    return "text";
+  };
+
+  if (name === "country")
+    return (
+      <FormControl variant="outlined" fullWidth size="small" margin="normal">
+        <InputLabel id="demo-simple-select-outlined-label">
+          {getLabel()}
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-outlined-label"
+          id={name}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label={getLabel()}
+        >
+          <MenuItem value={"Deutschland"}>Deutschland</MenuItem>
+          <MenuItem value={"Österreich"}>Österreich</MenuItem>
+        </Select>
+      </FormControl>
+    );
+
   return (
     <TextField
-      type={name === "password" ? "password" : "text"}
+      type={getType()}
       id={name}
       label={getLabel()}
       variant="outlined"
@@ -38,7 +84,6 @@ const InputField = (props) => {
       helperText={(meta.touched && meta.error) || ""}
       size="small"
       margin="normal"
-      // {...field}
     />
   );
 };
