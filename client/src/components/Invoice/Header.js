53cbaaab68026415543ab4ad1ddf5339f220e0a1
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2020-11-08 17:26:17 +0100

    adds Header component for invoice

---
+++ client/src/components/Invoice/Header.js
@@ -0,0 +1,118 @@
+import React from "react";
+import {
+  Document,
+  Page,
+  View,
+  Text,
+  Image,
+  Font,
+  StyleSheet,
+} from "@react-pdf/renderer";
+import moment from "moment";
+import logo from "../../logo.png";
+
+const BLACK_EIGHTY = "#333333";
+const MINTY_GREEN = "#559495";
+const BORDER_COLOR = "#000";
+const BORDER_STYLE = "solid";
+const BORDER_WIDTH = 0.5;
+const COL1_WIDTH = 25;
+const COL2_WIDTH = 53;
+const COL3_WIDTH = 10;
+const COL4_WIDTH = 12;
+const MAIN_MARGIN = 40;
+const BOTTOM_MARGIN_BETWEEN_SECTIONS = 30;
+const BOTTOM_MARGIN_WITHIN_SECTIONS = 15;
+const DEFAULT_FONT_SIZE = 11;
+
+const styles = StyleSheet.create({
+  header: {
+    marginBottom: BOTTOM_MARGIN_BETWEEN_SECTIONS,
+    fontFamily: "brandonLight",
+    lineHeight: 1.3,
+  },
+  logoContainer: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: "60px",
+    textAlign: "center",
+    marginBottom: BOTTOM_MARGIN_WITHIN_SECTIONS,
+  },
+  ownName: {
+    textTransform: "capitalize",
+    color: BLACK_EIGHTY,
+    marginBottom: BOTTOM_MARGIN_WITHIN_SECTIONS,
+    textAlign: "center",
+  },
+  ownAddress: {
+    color: MINTY_GREEN,
+    textAlign: "center",
+    marginBottom: BOTTOM_MARGIN_BETWEEN_SECTIONS,
+  },
+  customerTitle: {
+    fontFamily: "brandonMedium",
+  },
+  metaInfo: {
+    textAlign: "right",
+  },
+});
+
+const Header = (props) => {
+  const { userProfile, customer, formalInvoiceId } = props;
+  const {
+    firm,
+    zip,
+    street,
+    city,
+    country,
+    taxId,
+    lastName,
+    firstName,
+  } = customer;
+  const date = moment().format("DD|MM|YYYY");
+
+  const formatName = (name) => {
+    if (!name) return;
+    return name.toUpperCase().split("").join(" ");
+  };
+
+  return (
+    <View style={styles.header}>
+      <View style={styles.logoContainer}>
+        <Image src={logo} style={styles.logo} />
+      </View>
+      <Text style={styles.ownName}>{formatName(userProfile.name)}</Text>
+      <View style={styles.ownAddress}>
+        <Text>
+          {userProfile.street} • {userProfile.zip} {userProfile.city}
+        </Text>
+        <Text>{userProfile.email}</Text>
+        <Text>{userProfile.phone}</Text>
+      </View>
+      <View>
+        {firm && <Text style={styles.customerTitle}>{firm}</Text>}
+        {lastName && (
+          <Text style={styles.customerTitle}>
+            {firstName} {lastName}
+          </Text>
+        )}
+        <Text>{street}</Text>
+        <Text>
+          {zip} {city}
+        </Text>
+        <Text>{country}</Text>
+      </View>
+      <View style={styles.metaInfo}>
+        <Text>UID: {taxId}</Text>
+        <Text>Rechnungsnummer: {formalInvoiceId}</Text>
+        <Text>{date}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Header;
