b17394ed8c293d4674056b8e74738bedf8a7aee7
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-01-17 23:00:43 +0100

    adds illustration

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -3,9 +3,11 @@ import styled from "styled-components";
 import bottomWave from "../assets/bottom-wave.png";
 import topCloud from "../assets/top-cloud.png";
 import MainWidget from "../components/MainWidget";
+import illustration from "../assets/cheese-carrier.png";
 
 const MainPageLayout = styled.div`
-  background: url(${topCloud}) 20% 0% / 120% 60% no-repeat,
+  background: url(${illustration}) 20% 80% / auto 50% no-repeat,
+    url(${topCloud}) 20% 0% / 120% 60% no-repeat,
     url(${bottomWave}) 20% 100% / 100% 100% no-repeat;
   min-height: 100vh;
   width: 100vw;
